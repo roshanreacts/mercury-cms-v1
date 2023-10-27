6cbe392371ee9263cbecb3e1b41dacf596c1a9d6
Ramesh <ramesh.gorekanti@gmail.com>
2023-10-27 14:07:44 +0530

    addpage changes

--- src/container/addWebsite/index.jsx
+++ src/container/addWebsite/index.jsx
@@ -1,58 +1,74 @@
-import React from 'react';
+import React from "react";
 
 const AddWebsite = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div className="min-h-screen min-w-full bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 md:max-w-xl sm:mx-auto">
         <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-          <div className="max-w-md mx-auto">
+          <div className="max-w-lg mx-auto space-x-5">
             <div className="flex items-center space-x-5">
-              <div className="h-14 w-14 bg-yellow-200 rounded-full flex flex-shrink-0 justify-center items-center text-yellow-500 text-2xl font-mono">i</div>
-              <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                <h2 className="leading-relaxed">Create an Event</h2>
-                <p className="text-sm text-gray-500 font-normal leading-relaxed">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <div className="h-14 w-14 bg-gray-900 rounded-full flex flex-shrink-0 justify-center items-center text-white text-2xl font-mono">
+                w
+              </div>
+              <div className="block pl-2 font-semibold text-xl text-gray-700">
+                <h2 className="leading-relaxed">Create A Website</h2>
               </div>
             </div>
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div className="flex flex-col">
-                  <label className="leading-loose">Event Title</label>
-                  <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Event title" />
-                </div>
-                <div className="flex flex-col">
-                  <label className="leading-loose">Event Subtitle</label>
-                  <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Optional" />
-                </div>
-                <div className="flex items-center space-x-4">
-                  <div className="flex flex-col">
-                    <label className="leading-loose">Start</label>
-                    <div className="relative focus-within:text-gray-600 text-gray-400">
-                      <input type="text" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" />
-                      <div className="absolute left-3 top-2">
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                      </div>
-                    </div>
+            <div className="flex flex-col gap-4">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 gap-4">
+                <div className="flex gap-4">
+                  <div className="">
+                    <label className="leading-loose"> Website Slug</label>
+                    <input
+                      type="text"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="Website Slug"
+                    />
                   </div>
-                  <div className="flex flex-col">
-                    <label className="leading-loose">End</label>
-                    <div className="relative focus-within:text-gray-600 text-gray-400">
-                      <input type="text" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="26/02/2020" />
-                      <div className="absolute left-3 top-2">
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                      </div>
-                    </div>
+                  <div>
+                    <label className="leading-loose"> Website Name</label>
+                    <input
+                      type="text"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="Website Name"
+                    />
                   </div>
                 </div>
-                <div className="flex flex-col">
-                  <label className="leading-loose">Event Description</label>
-                  <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Optional" />
+                <div className="">
+                  <label className="leading-loose">Website Description</label>
+                  <textarea
+                    className='px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Description"'
+                    name="Description"
+                    id=""
+                    cols="20"
+                    rows="5"
+                  ></textarea>
+                </div>
+
+                <div className="flex gap-4">
+                  <div className="">
+                    <label className="leading-loose">Domain</label>
+                    <input
+                      type="text"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="Domain"
+                    />
+                  </div>
+                  <div className="">
+                    <label className="leading-loose">Status</label>
+                    <select
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                    >
+                      <option value="draft">Draft</option>
+                      <option value="published">Published</option>
+                    </select>
+                  </div>
                 </div>
               </div>
               <div className="pt-4 flex items-center space-x-4">
-                <button className="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none">
-                  <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg> Cancel
+                <button className="bg-gray-900 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
+                  Create
                 </button>
-                <button className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">Create</button>
               </div>
             </div>
           </div>
@@ -63,4 +79,5 @@ const AddWebsite = () => {
 };
 
 export default AddWebsite;
+
 
