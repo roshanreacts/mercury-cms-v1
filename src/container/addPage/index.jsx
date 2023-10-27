f87d3766e917f7a5e9e3b72b94ecaef1dbfc002b
Ramesh <ramesh.gorekanti@gmail.com>
2023-10-27 16:23:15 +0530

    addPage Changes

--- src/container/addPage/index.jsx
+++ src/container/addPage/index.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { RiFileAddLine } from 'react-icons/ri'
+"use client";
+import React from "react";
+import { RiFileAddLine } from "react-icons/ri";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
 const AddPage = () => {
+  const handleSubmit = (values, { setSubmitting }) => {
+    console.log(values);
+    setSubmitting(false);
+  };
+
+  const validationSchema = Yup.object().shape({
+    pageSlug: Yup.string().required("Required"),
+    pageTitle: Yup.string().required("Page Title is required"),
+    pageComponents: Yup.string().required("Page Components are required"),
+    metaDescription: Yup.string().required("Meta Description is required"),
+    pagePath: Yup.string().required("Page Path is required"),
+    version: Yup.number().required("Version is required"),
+  });
+
   return (
     <div className="min-h-screen min-w-full bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 md:max-w-xl sm:mx-auto">
         <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
           <div className="max-w-lg mx-auto space-x-5">
-            <div className="flex items-center space-x-5">
+            <div className="flex items-center text-center space-x-5 mb-6">
               <div className="h-14 w-14 bg-gray-900 rounded-full flex flex-shrink-0 justify-center items-center text-white text-2xl font-mono">
                 <RiFileAddLine />
               </div>
@@ -15,91 +32,137 @@ const AddPage = () => {
                 <h2 className="leading-relaxed">Create A Page</h2>
               </div>
             </div>
-            <div className="flex flex-col gap-4">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 gap-4">
-                <div className="flex gap-4">
+            <Formik
+              initialValues={{
+                pageSlug: "",
+                pageTitle: "",
+                pageComponents: "",
+                metaDescription: "",
+                pagePath: "",
+                status: "draft",
+                version: 1,
+              }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
                   <div className="">
                     <label className="leading-loose"> Page Slug</label>
-                    <input
+                    <Field
                       type="text"
+                      name="pageSlug"
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Page Slug"
+                    />
+                    <ErrorMessage
+                      name="pageSlug"
+                      component="div"
+                      className="text-red-500 text-xs"
                     />
                   </div>
                   <div className="">
                     <label className="leading-loose"> Page Title</label>
-                    <input
+                    <Field
                       type="text"
+                      name="pageTitle"
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Page Title"
                     />
+                    <ErrorMessage
+                      name="pageTitle"
+                      component="div"
+                      className="text-red-500 text-xs"
+                    />
                   </div>
-                </div>
-                <div className="">
+                  <div className="">
                     <label className="leading-loose">Page Components</label>
-                    <textarea
-                      className='px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Description"'
-                      name="Description"
-                      id=""
-                      cols="20"
+                    <Field
+                      as="textarea"
+                      name="pageComponents"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="Page Components"
                       rows="5"
-                    ></textarea>
+                    />
+                    <ErrorMessage
+                      name="pageComponents"
+                      component="div"
+                      className="text-red-500 text-xs"
+                    />
                   </div>
-                
-                  
                   <div className="">
                     <label className="leading-loose">Meta Description</label>
-                    <textarea
-                      className='px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Description"'
-                      name="Description"
-                      id=""
-                      cols="20"
+                    <Field
+                      as="textarea"
+                      name="metaDescription"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="Meta Description"
                       rows="5"
-                    ></textarea>
+                    />
+                    <ErrorMessage
+                      name="metaDescription"
+                      component="span"
+                      className="text-red-500 text-xs"
+                    />
                   </div>
                   <div className="">
                     <label className="leading-loose">Page Path</label>
-                    <input
+                    <Field
                       type="text"
+                      name="pagePath"
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Page Path"
                     />
-                  </div>
-                
-
-                <div className="flex gap-4">
-                  
-                  <div className="">
-                    <label className="leading-loose">Status</label>
-                    <select
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-900"
-                    >
-                      <option value="draft">Draft</option>
-                      <option value="active">Active</option>
-                    </select>
-                  </div>
-                  <div className="">
-                    <label className="leading-loose">Version</label>
-                    <input
-                      type="number"
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                      placeholder="Version"
+                    <ErrorMessage
+                      name="pagePath"
+                      component="span"
+                      className="text-red-500 text-xs"
                     />
                   </div>
-                </div>
-              </div>
-              <div className="pt-4 flex items-center space-x-4">
-                <button className="bg-gray-900 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
-                  Create
-                </button>
-              </div>
-            </div>
+                  <div className="flex gap-4">
+                    <div className="">
+                      <label className="leading-loose">Status</label>
+                      <Field
+                        as="select"
+                        name="status"
+                        className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-900"
+                      >
+                        <option value="draft">Draft</option>
+                        <option value="active">Active</option>
+                      </Field>
+                    </div>
+                    <div className="">
+                      <label className="leading-loose">Version</label>
+                      <Field
+                        type="number"
+                        name="version"
+                        className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                        placeholder="Version"
+                      />
+                      <ErrorMessage
+                        name="version"
+                        component="span"
+                        className="text-red-500 text-xs"
+                      />
+                    </div>
+                  </div>
+                  <div className="pt-4 flex items-center space-x-4">
+                    <button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="bg-gray-900 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
+                    >
+                      Create
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddPage
-
+export default AddPage;
--- src/container/addWebsite/index.jsx
+++ src/container/addWebsite/index.jsx
@@ -1,7 +1,29 @@
+"use client"
 import React from "react";
 import { BiAddToQueue } from "react-icons/bi";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const AddWebsite = () => {
+  const initialValues = {
+    websiteSlug: "",
+    websiteName: "",
+    websiteDescription: "",
+    domain: "",
+    status: "draft",
+  };
+
+  const validationSchema = Yup.object({
+    websiteSlug: Yup.string().required("Required"),
+    websiteName: Yup.string().required("Required"),
+    websiteDescription: Yup.string().required("Required"),
+    domain: Yup.string().required("Required"),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="min-h-screen min-w-full bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 md:max-w-xl sm:mx-auto">
@@ -9,69 +31,109 @@ const AddWebsite = () => {
           <div className="max-w-lg mx-auto space-x-5">
             <div className="flex items-center space-x-5">
               <div className="h-14 w-14 bg-gray-900 rounded-full flex flex-shrink-0 justify-center items-center text-white text-2xl font-mono">
-                <BiAddToQueue/>
+                <BiAddToQueue />
               </div>
               <div className="block pl-2 font-semibold text-xl text-gray-700">
                 <h2 className="leading-relaxed">Create A Website</h2>
               </div>
             </div>
-            <div className="flex flex-col gap-4">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 gap-4">
-                <div className="flex gap-4">
-                  <div className="">
-                    <label className="leading-loose"> Website Slug</label>
-                    <input
-                      type="text"
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                      placeholder="Website Slug"
-                    />
-                  </div>
-                  <div>
-                    <label className="leading-loose"> Website Name</label>
-                    <input
-                      type="text"
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                      placeholder="Website Name"
-                    />
-                  </div>
-                </div>
-                <div className="">
-                  <label className="leading-loose">Website Description</label>
-                  <textarea
-                    className='px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Description"'
-                    name="Description"
-                    id=""
-                    cols="20"
-                    rows="5"
-                  ></textarea>
-                </div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}
+            >
+              {({ touched, errors }) => (
+                <Form className="flex flex-col gap-4">
+                  <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 gap-4">
+                    <div className="flex gap-4">
+                      <div className="">
+                        <label className="leading-loose"> Website Slug</label>
+                        <Field
+                          type="text"
+                          name="websiteSlug"
+                          className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                          placeholder="Website Slug"
+                        />
+                        <ErrorMessage
+                          name="websiteSlug"
+                          component="span"
+                          className="text-red-500 text-xs"
+                        />
+                      </div>
+                      <div>
+                        <label className="leading-loose"> Website Name</label>
+                        <Field
+                          type="text"
+                          name="websiteName"
+                          className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                          placeholder="Website Name"
+                        />
+                        <ErrorMessage
+                          name="websiteName"
+                          component="div"
+                          className="text-red-500 text-xs"
+                        />
+                      </div>
+                    </div>
+                    <div className="">
+                      <label className="leading-loose">Website Description</label>
+                      <Field
+                        as="textarea"
+                        name="websiteDescription"
+                        className='px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Description"'
+                        placeholder="Description"
+                      />
+                      <ErrorMessage
+                        name="websiteDescription"
+                        component="div"
+                        className="text-red-500 text-xs"
+                      />
+                    </div>
 
-                <div className="flex gap-4">
-                  <div className="">
-                    <label className="leading-loose">Domain</label>
-                    <input
-                      type="text"
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                      placeholder="Domain"
-                    />
+                    <div className="flex gap-4">
+                      <div className="">
+                        <label className="leading-loose">Domain</label>
+                        <Field
+                          type="text"
+                          name="domain"
+                          className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                          placeholder="Domain"
+                        />
+                        <ErrorMessage
+                          name="domain"
+                          component="div"
+                          className="text-red-500 text-xs"
+                        />
+                      </div>
+                      <div className="">
+                        <label className="leading-loose">Status</label>
+                        <Field
+                          as="select"
+                          name="status"
+                          className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-900"
+                        >
+                          <option value="draft">Draft</option>
+                          <option value="published">Published</option>
+                        </Field>
+                        <ErrorMessage
+                          name="status"
+                          component="div"
+                          className="text-red-500 text-xs"
+                        />
+                      </div>
+                    </div>
                   </div>
-                  <div className="">
-                    <label className="leading-loose">Status</label>
-                    <select
-                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-900"
+                  <div className="pt-4 flex items-center space-x-4">
+                    <button
+                      type="submit"
+                      className="bg-gray-900 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
                     >
-                      <option value="draft">Draft</option>
-                      <option value="published">Published</option>
-                    </select>
+                      Create
+                    </button>
                   </div>
-                </div>
-              </div>
-              <div className="pt-4 flex items-center space-x-4">
-                <button className="bg-gray-900 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
-                  Create
-                </button>
-              </div>
-            </div>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
