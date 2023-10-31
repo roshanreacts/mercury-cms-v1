f4ae581b826a4707fb4283f25ecc6efb66d1dc2d
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2023-10-31 12:42:32 +0530

    useLoader

---
+++ src/container/useLoader.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+const useLoader = (promise) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    promise
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return { loading, error, data };
+};
+
+export default useLoader;
