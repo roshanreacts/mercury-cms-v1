16ffb5d1fae6730ba07fc49ab13c3c152c2fc30f
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-11-01 12:20:58 +0530

    changes in navbar conditional redering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
 import { FiLogOut } from "react-icons/fi";
 import Link from "next/link";
 import Image from "next/image";
+import { checkTokenExpiry } from "~/utilis/cookie";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+
+  const [loggedIn, setLoggedIn] = useState(!checkTokenExpiry()); 
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(!loggedIn){
+      router.push('/login');
+    }
+  }, [loggedIn])
+
   return (
     <div className="flex flex-wrap place-items-center w-full">
       <section className="relative mx-auto w-full">
@@ -19,25 +31,35 @@ const Navbar = () => {
               height={37}
               priority
             />
-            <div className="hidden xl:flex items-center space-x-5 ">
-              <a className="flex items-center hover:text-gray-200" href="#">
-                <BiUserCircle className="text-3xl" />
-              </a>
-              <div className="border-2 border-gray-200 p-1 rounded-md px-3 hover:text-gray-200 hidden xl:flex">
-                <button>{1 == 1 ? "Logout" : "Login"}</button>
+            {
+              loggedIn &&
+
+              <div className="hidden xl:flex items-center space-x-5 ">
+                <a className="flex items-center hover:text-gray-200" href="#">
+                  <BiUserCircle className="text-3xl" />
+                </a>
+                <div className="border-2 border-gray-200 p-1 rounded-md px-3 hover:text-gray-200 hidden xl:flex">
+                  <button>{1 == 1 ? "Logout" : "Login"}</button>
+                </div>
               </div>
+            }
+          </div>
+          {
+            loggedIn &&
+            <a
+              className="flex items-center mr-6  hover:text-gray-200  xl:hidden"
+              href="#"
+            >
+              <BiUserCircle className="text-3xl" />
+            </a>
+          }
+          {
+            loggedIn &&
+            <div className="border-2 flex mr-6 items-center self-center border-gray-200 p-1 rounded-md hover:text-gray-200  xl:hidden h-fit">
+              {1 == 1 && <FiLogOut />}
+              {/* Later codition should be added */}
             </div>
-          </div>
-          <a
-            className="flex items-center mr-6  hover:text-gray-200  xl:hidden"
-            href="#"
-          >
-            <BiUserCircle className="text-3xl" />
-          </a>
-          <div className="border-2 flex mr-6 items-center self-center border-gray-200 p-1 rounded-md hover:text-gray-200  xl:hidden h-fit">
-            {1 == 1 && <FiLogOut />}
-            {/* Later codition should be added */}
-          </div>
+          }
         </nav>
       </section>
     </div>
