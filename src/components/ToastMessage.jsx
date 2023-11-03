d4ea408c2b525053a7f5b88e33636c47bc5dfe43
Ramesh <ramesh.gorekanti@gmail.com>
2023-11-03 18:25:31 +0530

    toastMessage Component Added

---
+++ src/components/ToastMessage.jsx
@@ -0,0 +1,80 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const ToastSuccessMessage = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ToastErrorMessage = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ToastDefaultMessage = (msg) => {
+  toast.info(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ToastInfoMessage = (msg) => {
+  toast.info(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ToastDangerMessage = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ToastWarnMessage = (msg) => {
+  toast.info(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
--- src/container/login/index.jsx
+++ src/container/login/index.jsx
@@ -1,119 +1,126 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import Link from 'next/link';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { useRouter } from 'next/navigation'
-import store from '~/store';
+"use client";
+import React, { useEffect, useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import Link from "next/link";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/navigation";
+import store from "~/store";
 import { observer } from "mobx-react-lite";
-import {clearTokenCookie, setTokenCookie } from '~/utilis/cookie';
-import { useLazyQuery } from '../hooks';
-import { LOGIN_USER } from '~/utilis/queries';
-import Loader from '../Loader';
-
-
+import { clearTokenCookie, setTokenCookie } from "~/utilis/cookie";
+import { useLazyQuery } from "../hooks";
+import { LOGIN_USER } from "~/utilis/queries";
+import Loader from "../Loader";
+import { ToastErrorMessage, ToastSuccessMessage } from "~/components/ToastMessage";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const LoginContainer = () => {
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [loginUser, { data, loading, error }] = useLazyQuery(store.login);
-    const router = useRouter();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [loginUser, { data, loading, error }] = useLazyQuery(store.login);
+  const router = useRouter();
 
-    useEffect(() => {
-        if (data) {
-            setTokenCookie()
-            router.replace('/admin');
-        }
+  useEffect(() => {
+    if (data) {
+      setTokenCookie();
+      ToastSuccessMessage("Login Success!!")
+      router.replace("/admin");
+    }
 
-        if (error) {
-            console.log("login error", error)
-        }
-    }, [data, error])
+    if (error) {
+      ToastErrorMessage(error.message)
+    }
+  }, [data, error]);
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object({
-            email: Yup.string().email('Invalid email address').required('Required'),
-            password: Yup.string().
-                required('Required')
-        }),
-        onSubmit: (values) => {
-            loginUser(LOGIN_USER, values, { cache: "no-store" });
-        },
-    });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string().required("Required"),
+    }),
+    onSubmit: (values) => {
+      loginUser(LOGIN_USER, values, { cache: "no-store" });
+    },
+  });
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
-    };
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
-
-
-
-    return (
-        <>
-            <form onSubmit={formik.handleSubmit}>
-                <div className="flex flex-col items-center justify-center bg-gray-200 h-[90vh]">
-
-
-                    <div className="flex flex-col items-center justify-center p-8 rounded-lg border-2 border-gray-900 min-w-[35%] bg-white" style={{ boxShadow: "8px 12px 32px 0px rgba(0,0,0,0.1)" }}>
-                        <h1 className="text-2xl font-bold">Mercury CMS Admin Login</h1>
-                        <div className="mt-4 w-[60%]">
-                            <input
-                                className="w-full p-2 border rounded border-gray-300"
-                                type="email"
-                                placeholder="Email"
-                                maxLength="50"
-                                name='email'
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                required
-                            />
-                            {formik.touched.email && formik.errors.email ? (
-                                <div className='text-red-700 text-sm'>{formik.errors.email}</div>
-                            ) : null}
-                        </div>
-                        <div className="mt-4 w-[60%]">
-                            <div className="relative">
-                                <input
-                                    className="w-full p-2 border rounded border-gray-300"
-                                    type={passwordVisible ? 'text' : 'password'}
-                                    value={formik.values.password}
-                                    onChange={formik.handleChange}
-                                    placeholder="Password"
-                                    name="password"
-                                    required
-                                />
-                                <span className="absolute right-4 top-3 cursor-pointer" onClick={togglePasswordVisibility}>
-                                    {passwordVisible ? <AiFillEyeInvisible /> : <AiFillEye />}
-                                </span>
-                                {formik.touched.password && formik.errors.password ? (
-                                    <div className='text-red-700 text-sm'>{formik.errors.password}</div>
-                                ) : null}
-                            </div>
-                        </div>
-                        <div className="mt-6 w-[30%]">
-                            <button disabled={loading} className="w-full p-2 bg-gray-900 text-white rounded flex justify-center items-center" type="submit">
-                                {!loading ? "Login" : <Loader type="info" size="small"/>}
-                            </button>
-
-
-                        </div>
-
-
-                        <div className="mt-4">
-                            <Link href="/forgotpassword" className="text-blue-700 text-sm">Forgot Password?</Link>
-                        </div>
-                    </div>
-
+  return (
+    <>
+    <ToastContainer/>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="flex flex-col items-center justify-center bg-gray-200 h-[90vh]">
+          <div
+            className="flex flex-col items-center justify-center p-8 rounded-lg border-2 border-gray-900 min-w-[35%] bg-white"
+            style={{ boxShadow: "8px 12px 32px 0px rgba(0,0,0,0.1)" }}
+          >
+            <h1 className="text-2xl font-bold">Mercury CMS Admin Login</h1>
+            <div className="mt-4 w-[60%]">
+              <input
+                className="w-full p-2 border rounded border-gray-300"
+                type="email"
+                placeholder="Email"
+                maxLength="50"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                required
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-red-700 text-sm">
+                  {formik.errors.email}
                 </div>
-            </form>
+              ) : null}
+            </div>
+            <div className="mt-4 w-[60%]">
+              <div className="relative">
+                <input
+                  className="w-full p-2 border rounded border-gray-300"
+                  type={passwordVisible ? "text" : "password"}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  placeholder="Password"
+                  name="password"
+                  required
+                />
+                <span
+                  className="absolute right-4 top-3 cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                >
+                  {passwordVisible ? <AiFillEyeInvisible /> : <AiFillEye />}
+                </span>
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-red-700 text-sm">
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+            <div className="mt-6 w-[30%]">
+              <button
+                disabled={loading}
+                className="w-full p-2 bg-gray-900 text-white rounded flex justify-center items-center"
+                type="submit"
+              >
+                {!loading ? "Login" : <Loader type="info" size="small" />}
+              </button>
+            </div>
 
-
-        </>
-    );
-}
+            <div className="mt-4">
+              <Link href="/forgotpassword" className="text-blue-700 text-sm">
+                Forgot Password?
+              </Link>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default observer(LoginContainer);
